f4c6f95fd2389870cd274c8089fa3a737121e0b8
danny <dannydorstijn1997@gmail.com>
2018-02-19 14:02:09 +0100

    Generation of waves and plane

--- Assets/WaterSurface.cs
+++ Assets/WaterSurface.cs
@@ -2,51 +2,75 @@
 
 public class WaterSurface : MonoBehaviour
 {
-
     [SerializeField]
     int size;
 
     [SerializeField]
-    float oscillatorSpeed;
+    float oscillatorStrength;
 
     [SerializeField]
-    int resolution;
+    int oscillatorSpeed;
 
     int step;
+
+    Mesh mesh;
 
 
 	// Use this for initialization
 	void Start ()
     {
+        mesh = GetComponent<MeshFilter>().mesh;
+        mesh.name = "Surface Fluid";
+
+        // Find origin 
+        // Make force (origin, time)
+        // For every vertex, if distance between origin and vertex is equal
+        // to time, then vertex.y += 1/time;
 
     }
 
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
-
         int sizePower = size*size;
 
-        Vector3[] verts = new Vector3[sizePower];
-        int[] tris = {3,1,0, 4,2,1, 6,4,3, 7,5,4,  3,4,1, 4,5,2, 6,7,4, 7,8,5};// new int[verts.Length * 3];
+        Vector3[] verts = new Vector3[(size + 1) * (size + 1)];
+        int[] tris = new int[sizePower * 6];
+        Vector2[] uv = new Vector2[verts.Length];
 
-        float y = Mathf.Cos(2 * Mathf.PI / resolution * step);
+        for (int i = 0; i < verts.Length; i++)
+        {
+            float yOffset = Mathf.Cos(2 * Mathf.PI / oscillatorSpeed * step + i) * oscillatorStrength;
+            float xOffset = Mathf.Sin(2 * Mathf.PI / oscillatorSpeed * step + i) * oscillatorStrength;
 
-        for (int i = 0; i < verts.Length; i++) {
-            float x = Mathf.Sin(2 * Mathf.PI / resolution * step + i);
-            verts[i] = new Vector3(i % size /*+ x*/, 0 /*+ y*/, i / size);
+            verts[i] = new Vector3(i % (size + 1) + xOffset, 0 + yOffset, i / (size + 1));
+            uv[i] = new Vector2((float)i % (float)(size+1) / (float)size, (float)i / (float)(size+1) / (float)size);
 
-            if (i % (size-1) == 0) {
-                tris[i*3] = i;
-                tris[i*3+1] = i + 1;
-                tris[i*3+2] = i + size;
+            if (i < sizePower - 2)
+            {
+                // Every quad is 6 points, first 3 points are the lower triangle
+                // second 3 points are bottom.
+
+                // Create lower triangle 
+                tris[i * 6] = i;
+                tris[i * 6 + 1] = i + size + 1;
+                tris[i * 6 + 2] = i + 1;
+
+                // Create upper triangle
+                tris[i * 6 + 3] = i + size + 1;
+                tris[i * 6 + 4] = i + size + 2;
+                tris[i * 6 + 5] = i + 1;
             }
         }
 
-        GetComponent<MeshFilter>().mesh.Clear();
-        GetComponent<MeshFilter>().mesh.vertices = verts;
-        GetComponent<MeshFilter>().mesh.triangles = tris;
-        GetComponent<MeshFilter>().mesh.RecalculateNormals();
+        mesh.Clear();
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+        mesh.uv = uv;
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
         step++;
 	}
 }
